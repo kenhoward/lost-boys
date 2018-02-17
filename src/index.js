8672d3aea189c79d63bc7cf88c978c8428181e8f
Kenny Howard <ken.chad.howard@gmail.com>
2018-02-17 00:30:31 -0700

    just plugging up everything to get moving

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return ( <h1>Buenas Tardes</h1> );
+}
+
+ReactDOM.render(<App />, document.querySelector('.lb-container'));
